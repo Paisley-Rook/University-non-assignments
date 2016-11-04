208dac7f8d23840401f66a9b4fc0ca676a8273e2
Paisley-Rook <samchinn@hotmail.com>
2016-11-04 03:33:09 +0000

    Add files via upload

---
+++ Andrejus.c
@@ -0,0 +1,67 @@
+int delayTime = 100;
+
+char morseCode[] = {".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.. "};
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  //initial 5s pause
+  delay(50 * delayTime);
+  
+    for (int characterIterator = 0; characterIterator < sizeof(morseLetters); characterIterator++) 
+		{
+			switch(morseCode[characterIterator])
+				{
+					case '.': shortBlink(); break;
+					case '-': longBlink(); break;
+					case ' ': pause(); break;
+					default: debug(); break;
+				}
+			delay(delayTime);
+		} 
+}
+
+
+
+
+/** outputs '.' **/
+void shortBlink(void) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(delayTime);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+/** outputs '-' **/
+void longBlink(void) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(4 * delayTime);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+/** outputs ' ' **/
+void pause(void) {
+  delay(2 * delayTime);
+}
+
+/** 1 second pause followed by 3 flashes followed by another 1 second pause indicate a non-morse code character**/
+void debug(void)
+	{
+		delay(1);
+		
+		digitalWrite(LED_BUILTIN, HIGH);
+		delay(delayTime);
+		digitalWrite(LED_BUILTIN, LOW);
+		
+		digitalWrite(LED_BUILTIN, HIGH);	
+		delay(delayTime);
+		digitalWrite(LED_BUILTIN, LOW);
+		
+		digitalWrite(LED_BUILTIN, HIGH);
+		delay(delayTime);
+		digitalWrite(LED_BUILTIN, LOW);
+		
+		delay(1);
+	}
+		
